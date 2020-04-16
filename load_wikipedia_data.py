48ba58a17157def86525a5481e42e247d8877d2a
ignaciovi <ignvaldelvira@gmail.com>
2020-04-16 22:05:41 +0100

    Add logging to load and transform

--- load_wikipedia_data.py
+++ load_wikipedia_data.py
@@ -1,31 +1,29 @@
 import psycopg2
-from config import config
 import re
+import logging
+from config import config
 
 def load_wikipedia_data(data):
     ''' Connect to the PostgreSQL database server '''
 
     conn = None
     try:
-        # read connection parameters
         params = config()
- 
-        # connect to the PostgreSQL server
+
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
-      
-        # create a cursor
+
         cur = conn.cursor()
-        
-        
+
         print('Inserting data:')   
         cur.execute('INSERT INTO wiki_timebox_data (data) VALUES ('{}')'.format(data))
         print('Data inserted')
        
-       # close the communication with the PostgreSQL
         cur.close()
         conn.commit()
+        logging.debug('Inserted into database: {}'.format(data))
     except (Exception, psycopg2.DatabaseError) as error:
+        logging.debug(error)
         print(error)
     finally:
         if conn is not None:
